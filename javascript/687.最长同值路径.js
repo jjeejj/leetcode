b2ba23d2ca8b8fd6bdae2b27e5ab4131b194ecb0
jjeejj <wenjunjiang1993@163.com>
2019-12-08 20:28:29 +0800

    687.最长同值路径

---
+++ javascript/687.最长同值路径.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=687 lang=javascript
+ *
+ * [687] 最长同值路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestUnivaluePath = function(root) {
+    
+};
+// @lc code=end
+
