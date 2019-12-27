c13314fce9572dc391af797724f59c02f179e12d
姜文俊 <jiangwenjun@globalegrow.com>
2019-12-27 14:40:11 +0800

    349 和 62

---
+++ javascript/349.两个数组的交集.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    
+};
+// @lc code=end
+
---
+++ javascript/62.不同路径.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+/**
+ * 解题思维：
+ * 使用递归的思想
+ * 递归公式：f(m,n) = f(m. n-1) + f(m-1, n)
+ * 终止条件: f(1,2) = 1, f(2,1) = 1 
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    
+};
+// @lc code=end
+
