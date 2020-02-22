31040fbacbb73dabd159f927108fe88780737b29
jjeejj <wenjunjiang1993@163.com>
2020-02-22 13:26:05 +0800

    189.旋转数组

---
+++ javascript/189.旋转数组.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 旋转数组
+ *
+ * https://leetcode-cn.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (39.84%)
+ * Likes:    493
+ * Dislikes: 0
+ * Total Accepted:    98.9K
+ * Total Submissions: 244.3K
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释: 
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ * 
+ * 说明:
+ * 
+ * 
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 要求使用空间复杂度为 O(1) 的 原地 算法。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// 利用反转数组的思维
+var rotate = function(nums, k) {
+    var len = nums.length;
+    if (len <= 1) {
+        return nums;
+    };
+    // 计算处移动的最小位数
+    k = k % len;
+    // 原数组反转
+    reverse(nums, 0, len-1);
+    // 在反转前 K 个数组
+    reverse(nums, 0, k-1);
+    // 反转后面的
+    reverse(nums, k, len-1);
+    return nums;
+};
+
+// 反转一个数组
+var reverse = function(arr, start, end) {
+    while (start < end) {
+        var temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    };
+};
+// @lc code=end
+
