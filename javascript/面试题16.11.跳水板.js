338c374c515fad2f6ac7dfcca2e404ce516675ea
jjeejj <wenjunjiang1993@163.com>
2020-03-07 21:45:24 +0800

    面试题16.11.跳水板

---
+++ javascript/面试题16.11.跳水板.js
@@ -0,0 +1,37 @@
+/** 
+你正在使用一堆木板建造跳水板。有两种类型的木板，其中长度较短的木板长度为shorter，长度较长的木板长度为longer。你必须正好使用k块木板。编写一个方法，生成跳水板所有可能的长度。
+
+返回的长度需要从小到大排列。
+
+示例：
+
+输入：
+shorter = 1
+longer = 2
+k = 3
+输出： {3,4,5,6}
+提示：
+
+0 < shorter <= longer
+0 <= k <= 100000
+*/
+
+/**
+ * @param {number} shorter
+ * @param {number} longer
+ * @param {number} k
+ * @return {number[]}
+ */
+var divingBoard = function(shorter, longer, k) {
+    var res = new Set(); // 返回的结果
+    if (k == 0) {
+        return [...res];
+    };
+    var sl = k,curVal;
+    while (sl >= 0) {
+        curVal = sl * shorter + longer * (k-sl);
+        res.add(curVal);
+        sl--;
+    };
+    return [...res];
+};
