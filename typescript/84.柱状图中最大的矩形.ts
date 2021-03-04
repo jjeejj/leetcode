84dc3a74c0da04ba8fa81b6f0c1d819073ef18bb
jjeejj <jwj@wenjunjiang.com>
2021-03-04 22:34:29 +0800

    84.柱状图中最大的矩形

--- typescript/84.柱状图中最大的矩形.ts
+++ typescript/84.柱状图中最大的矩形.ts
@@ -10,27 +10,45 @@ function largestRectangleArea(heights: number[]): number {
     return 0;
   }
   if (heights.length == 1) {
-    return heights[1];
+    return heights[0];
   }
   // 柱子左边第一个小于其高度的位置
   let leftMinHeightStrack = [];
-  // 柱子右边第一个小于其高度的位置
-  let rightMinHeightSreack = [];
   // 单调递增栈, 里面存的是索引位置
   let minStrack = [];
   for (let i = 0; i < heights.length; i++) {
-    while (minStrack.length > 0 && heights[minStrack[minStrack.length - 1]] >= heights[i]) {
-      rightMinHeightSreack[minStrack.pop()] = i;
+    while (
+      minStrack.length > 0 &&
+      heights[minStrack[minStrack.length - 1]] >= heights[i]
+    ) {
+      minStrack.pop();
     }
-    leftMinHeightStrack.push(minStrack.length > 0 ? minStrack[minStrack.length - 1] : -1);
+    leftMinHeightStrack[i] = minStrack.length
+      ? minStrack[minStrack.length - 1]
+      : -1;
+    minStrack.push(i);
+  }
+  // 柱子右边第一个小于其高度的位置
+  let rightMinHeightSreack = [];
+  minStrack.length = 0;
+  for (let i = heights.length - 1; i >= 0; i--) {
+    while (
+      minStrack.length > 0 &&
+      heights[minStrack[minStrack.length - 1]] >= heights[i]
+    ) {
+      minStrack.pop();
+    }
+    rightMinHeightSreack[i] = minStrack.length
+      ? minStrack[minStrack.length - 1]
+      : heights.length;
+
     minStrack.push(i);
   }
   // 计算每根柱子的最大面积
-  console.log("leftMinHeightStrack", leftMinHeightStrack);
-  console.log("rightMinHeightSreack", rightMinHeightSreack);
   let max = 0;
   for (let i = 0; i < heights.length; i++) {
-    let currMax = (rightMinHeightSreack[i] - leftMinHeightStrack[i] - 1) * heights[i];
+    let currMax =
+      (rightMinHeightSreack[i] - leftMinHeightStrack[i] - 1) * heights[i];
     if (currMax > max) {
       max = currMax;
     }
