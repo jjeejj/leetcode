be2789ef66a9e073a632dfe4a532b69223c1b2ee
jjeejj <jwj@wenjunjiang.com>
2021-07-24 22:05:44 +0800

    1893.检查是否区域内所有整数都被覆盖

---
+++ typescript/1893.检查是否区域内所有整数都被覆盖.ts
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=1893 lang=typescript
+ *
+ * [1893] 检查是否区域内所有整数都被覆盖
+ *
+ * https://leetcode-cn.com/problems/check-if-all-the-integers-in-a-range-are-covered/description/
+ *
+ * algorithms
+ * Easy (57.47%)
+ * Likes:    55
+ * Dislikes: 0
+ * Total Accepted:    17.8K
+ * Total Submissions: 30.1K
+ * Testcase Example:  '[[1,2],[3,4],[5,6]]\n2\n5'
+ *
+ * 给你一个二维整数数组 ranges 和两个整数 left 和 right 。每个 ranges[i] = [starti, endi] 表示一个从
+ * starti 到 endi 的 闭区间 。
+ *
+ * 如果闭区间 [left, right] 内每个整数都被 ranges 中 至少一个 区间覆盖，那么请你返回 true ，否则返回 false 。
+ *
+ * 已知区间 ranges[i] = [starti, endi] ，如果整数 x 满足 starti i ，那么我们称整数x 被覆盖了。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+ * 输出：true
+ * 解释：2 到 5 的每个整数都被覆盖了：
+ * - 2 被第一个区间覆盖。
+ * - 3 和 4 被第二个区间覆盖。
+ * - 5 被第三个区间覆盖。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：ranges = [[1,10],[10,20]], left = 21, right = 21
+ * 输出：false
+ * 解释：21 没有被任何一个区间覆盖。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 1 i i
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+// 暴力破解
+function isCovered(ranges: number[][], left: number, right: number): boolean {
+  let existArr: boolean[] = new Array(51).fill(false);
+  for (const range of ranges) {
+    for (let i = range[0]; i <= range[1]; i++) {
+      existArr[i] = true;
+    }
+  }
+  for (let j = left; j <= right; j++) {
+    if (!existArr[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 排序数组
+function isCovered1(ranges: number[][], left: number, right: number): boolean {
+  ranges.sort((a1, a2) => {
+    return a1[0] - a2[0];
+  });
+  for (const range of ranges) {
+    if (left >= range[0] && left <= range[1]) {
+      left = range[1] + 1;
+    }
+  }
+  return left > right;
+}
+// @lc code=end
