b6c578d93a92634666ab13c363eca660a59d95ab
jjeejj <jwj@wenjunjiang.com>
2021-08-16 18:12:51 +0800

    283.移动零:typescript

--- javascript/283.移动零.js
+++ javascript/283.移动零.js
@@ -15,19 +15,39 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    var j = 0; // 第一个非 0 元素的索引
-    for(var i = 0; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[j] = nums[i];
-            if (i != j) {
-                nums[i] = 0;
-            };
-            j++;
-        };
-    };
+var moveZeroes = function (nums) {
+  var j = 0; // 第一个非 0 元素的索引
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[j] = nums[i];
+      if (i != j) {
+        nums[i] = 0;
+      }
+      j++;
+    }
+  }
 };
 
 /**
  * 第二中方法 记录数组中的 0的个数
-*/
+ */
+var moveZeroes = function (nums) {
+  var zeroCount = 0; // 0 元素的个数
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      zeroCount++;
+    }
+  }
+  // 当前保存数据的位置 m = 0;
+  let m = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[m] = nums[j];
+      m++;
+    }
+  }
+  while (m < nums.length) {
+    nums[m] = 0;
+    m++;
+  }
+};
