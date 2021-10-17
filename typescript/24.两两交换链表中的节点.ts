653b7669b16bcc9571fd25b2c8e2eefc4a67adf7
jjeejj <jwj@wenjunjiang.com>
2021-10-17 23:08:47 +0800

    24.两两交换链表中的节点

---
+++ typescript/24.两两交换链表中的节点.ts
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=typescript
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (70.00%)
+ * Likes:    1078
+ * Dislikes: 0
+ * Total Accepted:    324.1K
+ * Total Submissions: 460.7K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0
+ *
+ *
+ *
+ *
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ *
+ */
+
+// @lc code=start
+
+//  Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function swapPairs(head: ListNode | null): ListNode | null {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  let sentry = new ListNode(1);
+  sentry.next = head;
+  let cur = head,
+    pre = sentry,
+    next = null;
+  while (cur != null && cur.next != null) {
+    next = cur.next.next;
+
+    pre.next = cur.next;
+    cur.next.next = cur;
+    cur.next = next;
+    pre = cur;
+    cur = next!;
+  }
+  return sentry.next;
+}
+// @lc code=end
