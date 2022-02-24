8e8722abf2e834ccd445d949282a377e83b19919
jjeejj <jwj@wenjunjiang.com>
2022-02-24 16:58:07 +0800

    145.二叉树的后序遍历

---
+++ go/145.二叉树的后序遍历.go
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=golang
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (74.77%)
+ * Likes:    762
+ * Dislikes: 0
+ * Total Accepted:    371.1K
+ * Total Submissions: 492K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[3,2,1]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) (res []int) {
+	var postorder func(node *TreeNode)
+	postorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		postorder(node.Left)
+		postorder(node.Right)
+		res = append(res, node.Val)
+	}
+	postorder(root)
+	return
+}
+
+// @lc code=end
+
