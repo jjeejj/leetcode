89954d9744d71c2999962911033cf335a72d999c
jjeejj <jwj@wenjunjiang.com>
2022-02-25 10:22:46 +0800

    119.杨辉三角-ii

---
+++ go/119.杨辉三角-ii.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=golang
+ *
+ * [119] 杨辉三角 II
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (65.64%)
+ * Likes:    369
+ * Dislikes: 0
+ * Total Accepted:    176.4K
+ * Total Submissions: 260.2K
+ * Testcase Example:  '3'
+ *
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: rowIndex = 3
+ * 输出: [1,3,3,1]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: rowIndex = 0
+ * 输出: [1]
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: rowIndex = 1
+ * 输出: [1,1]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 0
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？
+ *
+ */
+package main
+
+func getRow(rowIndex int) []int {
+	C := make([][]int, rowIndex+1)
+	for i, _ := range C {
+		C[i] = make([]int, i+1)
+		// 每一行的第一项和最后一项都是1
+		C[i][0], C[i][i] = 1, 1
+		// 从 rowIndex = 2开始 即物理上的第三行开始计算其他每一项的值
+		for j := 1; j < i; j++ {
+			C[i][j] = C[i-1][j-1] + C[i-1][j]
+		}
+	}
+	return C[rowIndex]
+}
+
+// 优化使用一维数组
+func getRow1(rowIndex int) []int {
+	var pre, cur []int
+	for i := 0; i <= rowIndex; i++ {
+		cur = make([]int, i+1)
+		cur[0], cur[i] = 1, 1
+		for j := 1; j < i; j++ {
+			cur[j] = pre[j-1] + pre[j]
+		}
+		pre = cur
+	}
+	return cur
+}
