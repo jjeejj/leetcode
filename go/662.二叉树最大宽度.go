d9c7adeaff08c101d0eee8fe06fedd9fa9f7b769
姜文俊 <wenjunjiang@aquanliang.com>
2022-08-27 22:57:05 +0800

    662.二叉树最大宽度

---
+++ go/662.二叉树最大宽度.go
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode.cn id=662 lang=golang
+ *
+ * [662] 二叉树最大宽度
+ *
+ * https://leetcode-cn.com/problems/maximum-width-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (40.25%)
+ * Likes:    482
+ * Dislikes: 0
+ * Total Accepted:    70.9K
+ * Total Submissions: 164.6K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
+ *
+ * 树的 最大宽度 是所有层中最大的 宽度 。
+ *
+ *
+ *
+ * 每一层的 宽度 被定义为该层最左和最右的非空节点（即，两个端点）之间的长度。将这个二叉树视作与满二叉树结构相同，两端点间会出现一些延伸到这一层的
+ * null 节点，这些 null 节点也计入长度。
+ *
+ * 题目数据保证答案将会在  32 位 带符号整数范围内。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,3,2,5,3,null,9]
+ * 输出：4
+ * 解释：最大宽度出现在树的第 3 层，宽度为 4 (5,3,null,9) 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,3,2,5,null,null,9,6,null,7]
+ * 输出：7
+ * 解释：最大宽度出现在树的第 4 层，宽度为 7 (6,null,null,null,null,null,7) 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,3,2,5]
+ * 输出：2
+ * 解释：最大宽度出现在树的第 2 层，宽度为 2 (3,2) 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目范围是 [1, 3000]
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+type pair struct {
+	node  *TreeNode
+	index int
+}
+
+func widthOfBinaryTree(root *TreeNode) int {
+	ans := 1
+	q := []pair{{root, 1}}
+	for q != nil {
+		// 计算每一场和之前最大值进比较
+		ans = max(ans, q[len(q)-1].index-q[0].index+1)
+		temp := q
+		q = nil
+		for _, p := range temp {
+			if p.node.Left != nil {
+				q = append(q, pair{p.node.Left, p.index * 2})
+			}
+			if p.node.Right != nil {
+				q = append(q, pair{p.node.Right, p.index*2 + 1})
+			}
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
